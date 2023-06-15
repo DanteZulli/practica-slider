c3e60f24cf97546bda1cb50d0f4f18f7b4e9f105
Dante Zulli <dantezulli2004@gmail.com>
2023-06-15 13:51:26 -0300

    comenté lo de JS y agregué bibliografía

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,26 @@
+// *****************
+// SLIDER
+// *****************
+// Bibliografía:
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollLeft
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollWidth
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/clientWidth
+// https://www.youtube.com/watch?v=7HPsdVQhpRw
+
+// Variables
 const slider = document.querySelector(".slider");
 const firstImg = document.querySelector(".slider img:first-child");
-
 let isDragStart = false,
   prevPageX,
   prevScrollLeft,
   positionDiff;
 
+// Funciones
 const autoSlide = () => {
-  positionDiff = Math.abs(positionDiff);
+  positionDiff = Math.abs(positionDiff); // La paso a positivo
   let firstImgWidth = firstImg.clientWidth;
   let valDifference = firstImgWidth - positionDiff;
-  //Right and Left
+  // Derecha e Izquierda
   if (slider.scrollLeft > prevScrollLeft) {
     if (positionDiff > firstImgWidth / 3) {
       return (slider.scrollLeft += valDifference);
@@ -26,6 +36,7 @@ const autoSlide = () => {
   }
 };
 
+// Eventos
 const dragStart = (e) => {
   isDragStart = true;
   prevPageX = e.pageX || e.touches[0].pageX;
@@ -34,10 +45,10 @@ const dragStart = (e) => {
 
 const dragging = (e) => {
   if (!isDragStart) return;
-  e.preventDefault();
-  slider.scrollLeft = e.pageX || e.touches[0].pageX;
+  e.preventDefault(); // Evita que se seleccione la imagen 
+  slider.scrollLeft = e.pageX || e.touches[0].pageX; 
   positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  slider.scrollLeft = prevScrollLeft - positionDiff;
+  slider.scrollLeft = prevScrollLeft - positionDiff; 
 };
 
 const dragEnd = (e) => {
@@ -45,9 +56,11 @@ const dragEnd = (e) => {
   autoSlide();
 };
 
+// Event Listeners
 slider.addEventListener("mousedown", dragStart);
 slider.addEventListener("mousemove", dragging);
 slider.addEventListener("mouseup", dragEnd);
+
 slider.addEventListener("touchstart", dragStart);
 slider.addEventListener("touchmove", dragging);
 slider.addEventListener("touchend", dragEnd);
